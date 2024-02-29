52c94903a54e49832f912cb36caf1c580d614a0c
Alessandro Neri <alessandro.neri@maggioli.it>
2024-02-29 12:55:08 +0100

    Plugin cleanup & keymap refactor

--- lua/theprimeagen/lazy/harpoon.lua
+++ lua/theprimeagen/lazy/harpoon.lua
@@ -8,7 +8,7 @@ return {
 
             harpoon:setup()
 
-            vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
+            vim.keymap.set("n", "<leader>ht", function() harpoon:list():append() end)
             vim.keymap.set("n", "<leader>ha", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
             vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
--- lua/theprimeagen/lazy/telescope.lua
+++ lua/theprimeagen/lazy/telescope.lua
@@ -11,7 +11,7 @@ return {
         require('telescope').setup({})
 
         local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+        vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
         vim.keymap.set('n', '<C-p>', builtin.git_files, {})
         vim.keymap.set('n', '<leader>pws', function()
             local word = vim.fn.expand("<cword>")
@@ -21,10 +21,9 @@ return {
             local word = vim.fn.expand("<cWORD>")
             builtin.grep_string({ search = word })
         end)
-        vim.keymap.set('n', '<leader>ps', function()
+        vim.keymap.set('n', '<leader>sf', function()
             builtin.grep_string({ search = vim.fn.input("Grep > ") })
         end)
-        vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+        vim.keymap.set('n', '<leader>ht', builtin.help_tags, {})
     end
 }
-
