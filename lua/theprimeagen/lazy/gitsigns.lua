b84c6e878dd985fcb985f670c86009aff6eb28b5
Alessandro Neri <alessandro.neri@maggioli.it>
2024-03-05 10:50:06 +0100

    fix: trouble signs in signcolumn

--- lua/theprimeagen/lazy/gitsigns.lua
+++ lua/theprimeagen/lazy/gitsigns.lua
@@ -4,7 +4,37 @@ return {
         event = "VeryLazy",
         config = function()
             require("gitsigns").setup({
+                max_file_length = 100000,
+                --               signs = {
+                --   add = { text = '+' },
+                --   change = { text = '~' },
+                --   delete = { text = '_' },
+                --   topdelete = { text = '‾' },
+                --   changedelete = { text = '~' },
+                -- },
+                signs = {
+                    add          = { show_count = false },
+                    change       = { show_count = false },
+                    delete       = { show_count = true },
+                    topdelete    = { show_count = true },
+                    changedelete = { show_count = true },
+                },
+                preview_config = {
+                    border = 'rounded',
+                },
                 current_line_blame = false,
+                current_line_blame_formatter_opts = {
+                    relative_time = true
+                },
+                current_line_blame_opts = {
+                    delay = 50
+                },
+                count_chars = {
+                    '⒈', '⒉', '⒊', '⒋', '⒌', '⒍', '⒎', '⒏', '⒐',
+                    '⒑', '⒒', '⒓', '⒔', '⒕', '⒖', '⒗', '⒘', '⒙', '⒚', '⒛',
+                },
+                _signs_staged_enable = true,
+                trouble = true,
                 on_attach = function(bufnr)
                     local gs = package.loaded.gitsigns
 
@@ -32,9 +62,11 @@ return {
                     map({ 'n', 'v' }, '<leader>hr', ':Gitsigns reset_hunk<CR>')
                     map('n', '<leader>hS', gs.stage_buffer)
                     map('n', '<leader>hs', gs.stage_hunk)
+                    map('n', '<leader>hr', gs.reset_hunk)
                     map('n', '<leader>hu', gs.undo_stage_hunk)
                     map('n', '<leader>hR', gs.reset_buffer)
                     map('n', '<leader>hp', gs.preview_hunk)
+                    map('n', '<leader>hi', gs.preview_hunk_inline)
                     map('n', '<leader>hb', function() gs.blame_line { full = true } end)
                     map('n', '<leader>tb', gs.toggle_current_line_blame)
                     map('n', '<leader>hd', gs.diffthis)
--- lua/theprimeagen/lazy/trouble.lua
+++ lua/theprimeagen/lazy/trouble.lua
@@ -2,22 +2,31 @@ return {
     {
         "folke/trouble.nvim",
         config = function()
-            require("trouble").setup({
-                icons = false,
-            })
+            require("trouble").setup()
 
             vim.keymap.set("n", "<leader>tt", function()
                 require("trouble").toggle()
             end)
 
             vim.keymap.set("n", "[t", function()
-                require("trouble").next({skip_groups = true, jump = true});
+                require("trouble").next({ skip_groups = true, jump = true });
             end)
 
             vim.keymap.set("n", "]t", function()
-                require("trouble").previous({skip_groups = true, jump = true});
+                require("trouble").previous({ skip_groups = true, jump = true });
             end)
-
+            -- Diagnostic signs
+            -- https://github.com/folke/trouble.nvim/issues/52
+            local signs = {
+                Error = " ",
+                Warning = " ",
+                Hint = " ",
+                Information = " "
+            }
+            for type, icon in pairs(signs) do
+                local hl = "DiagnosticSign" .. type
+                vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+            end
         end
     },
 }
