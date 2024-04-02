da5948521b34e12ed607443cef2ec8539bcbf637
Alessandro Neri <alessandro.neri@maggioli.it>
2024-04-02 16:33:23 +0200

    feat: transizione a lsp-zero

--- lua/theprimeagen/lazy/lsp.lua
+++ lua/theprimeagen/lazy/lsp.lua
@@ -1,96 +1,97 @@
-return {
-    "neovim/nvim-lspconfig",
-    event = { "BufReadPost" },
-    cmd = { "LspInfo", "LspInstall", "LspUninstall", "Mason" },
-    dependencies = {
-        -- LSP Manager
-        "williamboman/mason.nvim",
-        "williamboman/mason-lspconfig.nvim",
-        -- Autocompletion
-        "hrsh7th/cmp-nvim-lsp",
-        -- Progress/Status update for LSP
-        "j-hui/fidget.nvim",
-    },
-
-    config = function()
-        local cmp_lsp = require("cmp_nvim_lsp")
-        local capabilities = vim.tbl_deep_extend(
-            "force",
-            {},
-            vim.lsp.protocol.make_client_capabilities(),
-            cmp_lsp.default_capabilities())
-
-        require("fidget").setup({})
-
-        -- Setup mason so it can manage 3rd party LSP servers
-        require("mason").setup({
-            ui = {
-                border = "rounded",
-            },
-        })
-
-        -- Configure mason to auto install servers configured in lspconfig
-        require("mason-lspconfig").setup({
-            automatic_installation = { exclude = { "ocamllsp", "gleam" } },
-            ensure_installed = {
-                "lua_ls",
-                "rust_analyzer",
-                "tsserver",
-                "pyright",
-                "gopls",
-                "terraformls",
-            },
-            handlers = {
-                function(server_name) -- default handler (optional)
-                    require("lspconfig")[server_name].setup {
-                        capabilities = capabilities
-                    }
-                end,
-
-                ["pyright"] = function()
-                    local lspconfig = require("lspconfig")
-                    lspconfig.pyright.setup {
-                        capabilities = capabilities,
-                        settings = {
-                            pyright = { autoImportCompletion = true, },
-                            python = {
-                                analysis = {
-                                    autoSearchPaths = true,
-                                    diagnosticMode = 'openFilesOnly',
-                                    useLibraryCodeForTypes = true,
-                                    typeCheckingMode = 'off'
-                                }
-                            }
-                        }
-                    }
-                end,
-
-                ["lua_ls"] = function()
-                    local lspconfig = require("lspconfig")
-                    lspconfig.lua_ls.setup {
-                        capabilities = capabilities,
-                        settings = {
-                            Lua = {
-                                diagnostics = {
-                                    globals = { "vim", "it", "describe", "before_each", "after_each" },
-                                }
-                            }
-                        }
-                    }
-                end,
-            }
-        })
-
-        vim.diagnostic.config({
-            -- update_in_insert = true,
-            float = {
-                focusable = false,
-                style = "minimal",
-                border = "rounded",
-                source = "always",
-                header = "",
-                prefix = "",
-            },
-        })
-    end
-}
+return {}
+-- return {
+--     "neovim/nvim-lspconfig",
+--     event = { "BufReadPost" },
+--     cmd = { "LspInfo", "LspInstall", "LspUninstall", "Mason" },
+--     dependencies = {
+--         -- LSP Manager
+--         "williamboman/mason.nvim",
+--         "williamboman/mason-lspconfig.nvim",
+--         -- Autocompletion
+--         "hrsh7th/cmp-nvim-lsp",
+--         -- Progress/Status update for LSP
+--         "j-hui/fidget.nvim",
+--     },
+--
+--     config = function()
+--         local cmp_lsp = require("cmp_nvim_lsp")
+--         local capabilities = vim.tbl_deep_extend(
+--             "force",
+--             {},
+--             vim.lsp.protocol.make_client_capabilities(),
+--             cmp_lsp.default_capabilities())
+--
+--         require("fidget").setup({})
+--
+--         -- Setup mason so it can manage 3rd party LSP servers
+--         require("mason").setup({
+--             ui = {
+--                 border = "rounded",
+--             },
+--         })
+--
+--         -- Configure mason to auto install servers configured in lspconfig
+--         require("mason-lspconfig").setup({
+--             automatic_installation = { exclude = { "ocamllsp", "gleam" } },
+--             ensure_installed = {
+--                 "lua_ls",
+--                 "rust_analyzer",
+--                 "tsserver",
+--                 "pyright",
+--                 "gopls",
+--                 "terraformls",
+--             },
+--             handlers = {
+--                 function(server_name) -- default handler (optional)
+--                     require("lspconfig")[server_name].setup {
+--                         capabilities = capabilities
+--                     }
+--                 end,
+--
+--                 ["pyright"] = function()
+--                     local lspconfig = require("lspconfig")
+--                     lspconfig.pyright.setup {
+--                         capabilities = capabilities,
+--                         settings = {
+--                             pyright = { autoImportCompletion = true, },
+--                             python = {
+--                                 analysis = {
+--                                     autoSearchPaths = true,
+--                                     diagnosticMode = 'openFilesOnly',
+--                                     useLibraryCodeForTypes = true,
+--                                     typeCheckingMode = 'off'
+--                                 }
+--                             }
+--                         }
+--                     }
+--                 end,
+--
+--                 ["lua_ls"] = function()
+--                     local lspconfig = require("lspconfig")
+--                     lspconfig.lua_ls.setup {
+--                         capabilities = capabilities,
+--                         settings = {
+--                             Lua = {
+--                                 diagnostics = {
+--                                     globals = { "vim", "it", "describe", "before_each", "after_each" },
+--                                 }
+--                             }
+--                         }
+--                     }
+--                 end,
+--             }
+--         })
+--
+--         vim.diagnostic.config({
+--             -- update_in_insert = true,
+--             float = {
+--                 focusable = false,
+--                 style = "minimal",
+--                 border = "rounded",
+--                 source = "always",
+--                 header = "",
+--                 prefix = "",
+--             },
+--         })
+--     end
+-- }
