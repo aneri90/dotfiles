89c7b8461c89d87fbce2011be2482fad00faa5a5
Alessandro Neri <alessandro.neri@maggioli.it>
2024-04-02 14:58:23 +0200

    feat: added gitlab duo plugin

---
+++ lua/theprimeagen/lazy/gitlab.lua
@@ -0,0 +1,31 @@
+return {
+    {
+        url = 'https://gitlab.com/gitlab-org/editor-extensions/gitlab.vim.git',
+        event = {
+            'BufReadPre',
+            'BufNewFile',
+        },
+        ft = {
+            'go',
+            'javascript',
+            'python',
+            'ruby',
+        },
+        cond = function()
+            return vim.env.GITLAB_TOKEN ~= nil and vim.env.GITLAB_TOKEN ~= ''
+        end,
+        opts = {
+            minimal_message_level = vim.lsp.log_levels.ERROR,
+            statusline = {
+                enabled = false,
+            },
+            code_suggestions = {
+                -- Required in order to avoid "multiple different client offset_encodings
+                -- detected for buffer, this is not supported yet" warning due to other LSP
+                -- clients using UTF-16
+                offset_encoding = 'utf-16',
+            },
+        },
+    },
+}
+
