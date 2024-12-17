18f7e301eba6c79af6c89656dd2833bc9699ce7d
Alessandro Neri <alessandro.neri@maggioli.it>
2024-12-17 10:17:55 +0100

    fix: bump neovim packages & update deprecated lsp servers

--- nvim/lua/plugins/lazy/lsp.lua
+++ nvim/lua/plugins/lazy/lsp.lua
@@ -139,7 +139,7 @@ return {
 				docker_compose_language_service = {},
 				dockerls = {},
 				bashls = {},
-				tsserver = {},
+				ts_ls = {},
 			},
 			-- you can do any additional lsp server setup here
 			-- return true if you don't want this server to be setup with lspconfig
