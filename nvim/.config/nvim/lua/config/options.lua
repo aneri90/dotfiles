fe71b15b4f1e96f539912f90a3293b957efdf1c7
Alessandro Neri <alessandro.neri@maggioli.it>
2026-02-23 13:43:18 +0100

    feat: support win clipboard in wsl with win32yank

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,3 +1,21 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- WSL clipboard support via win32yank
+-- On macOS, Neovim auto-detects pbcopy/pbpaste
+-- On native Linux, Neovim auto-detects xclip/xsel/wl-copy
+if vim.fn.has("wsl") == 1 then
+  vim.g.clipboard = {
+    name = "win32yank-wsl",
+    copy = {
+      ["+"] = "win32yank.exe -i --crlf",
+      ["*"] = "win32yank.exe -i --crlf",
+    },
+    paste = {
+      ["+"] = "win32yank.exe -o --lf",
+      ["*"] = "win32yank.exe -o --lf",
+    },
+    cache_enabled = 0,
+  }
+end
